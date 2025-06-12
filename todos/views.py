33ebec5a338a1c10a71bf21e6504caa0f1712d24
Nome do Aluno <oliveiraabreu171@gmail.com>
2025-06-12 19:44:43 -0300

    questao 2 sem html

--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views.generic import ListView, CreateView, UpdateView
+from .models import Todo
+from django.urls import reverse_lazy
+
+class TodoListView(ListView):
+    model = Todo
+
+class TodoCreateView(CreateView):
+    model = Todo
+    fields = ['title', 'deadline']
+    success_url = reverse_lazy('todo_list')
+
+class TodoUpdateView(CreateView):
+    model = Todo
+    fields = ['title', 'deadline']
+    success_url = reverse_lazy('todo_list')
+
+
 
 # Create your views here.
